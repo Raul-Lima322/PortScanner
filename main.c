1068add8b043db3b4fbfb0f7705b9d5aba43a19f
Raul-Lima322 <raulpinheirolima@gmail.com>
2026-07-16 14:03:26 -0300

    Agora o programa envia uma mensagem para as portas abertas

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/time.h>
 
 
 int main(){
@@ -24,6 +25,11 @@ int main(){
             exit(EXIT_FAILURE);
         }
 
+        struct timeval timeout;
+        timeout.tv_sec = 1;
+        timeout.tv_usec =0;
+        setsockopt(socket_rede, SOL_SOCKET,SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
+
         struct sockaddr_in alvo;
         memset(&alvo, 0, sizeof(alvo));
         alvo.sin_family = AF_INET;
@@ -37,18 +43,27 @@ int main(){
         
         int conexao_ip = connect(socket_rede, (struct sockaddr *)&alvo, sizeof(alvo));
 
-        if (conexao_ip == 0)
-        {
-            printf("[*] Porta %d: aberta\n", porta);
-            conexoes +=1;
-        }
-        
-        close(socket_rede);
-    }
-    
-    printf("\n[-] Quantidade de portas abertas: %d\n", conexoes);
-    return 0;
-}
-/*comando para abrir uma porta:     sudo nc -lk 127.0.0.1 {n da porta}          ex: sudo nc -lk 127.0.0.1 80
+if (conexao_ip == 0)
+{
+    printf("[*] Porta %d: aberta!\n", porta);
+    conexoes += 1;
 
-*/
+    const char *mensagem_usuario = 
+    " ____            _   ____                             \n"
+        "|  _ \\ ___  _ __| |_/ ___|  ___ __ _ _ __  _ __   ___ _ __\n"
+        "| |_) / _ \\| '__| __\\___ \\ / __/ _` | '_ \\| '_ \\ / _ \\ '__|\n"
+        "|  __/ (_) | |  | |_ ___) | (_| (_| | | | | | | |  __/ |   \n"
+        "|_|   \\___/|_|   \\__|____/ \\___\\__,_|_| |_|_| |_|\\___|_|  \n"
+        "\n                       v1.1\n";
+
+    send(socket_rede, mensagem_usuario, strlen(mensagem_usuario), 0);
+    printf("    [->] Mensagem enviada!\n");
+
+    char buffer_resposta[1024];
+    memset(buffer_resposta, 0, sizeof(buffer_resposta));   
+    printf("\n");
+
+        }
+    }
+}
+//comando para abrir uma porta:     sudo nc -lk 127.0.0.1 {n da porta}          ex: sudo nc -lk 127.0.0.1 80
